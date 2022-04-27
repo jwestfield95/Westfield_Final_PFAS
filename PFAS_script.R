381412a7a923555618bdecf6eb48b2b2c39bcc1f
Jacob Westfield <jacob_westfield@uri.edu>
2022-04-27 17:32:47 -0400

    final edits to script

--- PFAS_script.R
+++ PFAS_script.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(title)
 library(moderndive)
 
-#Code to Access the Data
+#Code to Access the Data 
 All_Data_pfas <- read.csv("~/Westfieldfinalproject/pfas/All_Data_pfas.csv")
 
 ########################################################################################
@@ -28,7 +28,6 @@ FP_INF_EFF<-rbind(FP_EFFLUENT,FP_INFLUENT)
 #graph of FP INF AND EFF reportable PFAS compound concentrations 
 ggplot(FP_INF_EFF,mapping=aes(Description,textResult,fill=Description))+geom_col()+
   facet_wrap(~Compound)+scale_x_discrete(limits=c("FP Influent","FP Final Effluent"))+
- labs(title="Avg. Reportable Conc.(ng/L) of PFAS compounds at Fields Point from Oct. 2020 to May 2021")+
     ylab("Avg. Concenration (ng/L)")+xlab(" ")+theme(legend.position = "none")+theme(axis.text.x = element_text(angle = -90))
 
 
@@ -40,7 +39,7 @@ summaryFP<-get_regression_summaries(model_FP)%>% pull(p_value)
 #######################################################################
 #BUCKLIN POINT
 #most code is the same except filtering for Bucklin Point instead of Fields Point
-#taking fp influent and effluent reportable avg concentrations.
+#taking Bucklin Point influent and effluent reportable avg concentrations.
 BP_INFLUENT<-All_Data_pfas %>% filter(Description=="BP Influent") %>% filter(Qualifier==" ") %>%    
   group_by(Description,Compound,Plant) %>% 
   summarize(textResult= mean(textResult))
@@ -52,7 +51,6 @@ BP_INF_EFF<-rbind(BP_EFFLUENT,BP_INFLUENT)
 #Graph of BP INF and EFF avg reportable pfas compound concentrations. 
 ggplot(BP_INF_EFF,mapping=aes(Description,textResult,fill=Description))+geom_col()+
   facet_wrap(~Compound)+scale_x_discrete(limits=c("BP Influent","BP Final Effluent"))+
-  labs(title="Avg. Reportable Conc.(ng/L) of PFAS compounds at Bucklin Point from Oct. 2020 to May 2021")+
   ylab("Avg. Concenration (ng/L)")+xlab(" ")+theme(legend.position = "none")+theme(axis.text.x = element_text(angle = -90))
 
 #check to see if avg conc. is a function of INFULENT and EFFLUENT
@@ -72,31 +70,40 @@ FP_BP_FCAK<-rbind(FP_FCAK,BP_FCAK)
 #graph Avg. Reportable Conc.(ug/Kg) of PFAS compounds in Fields Point and Bucklin Point Filter Cake from October 2020 to May 2021     
 ggplot(FP_BP_FCAK,mapping=aes(Description,textResult,fill=Description))+geom_col()+
   facet_wrap(~Compound)+scale_x_discrete(limits=c("FP Filter Cake","BP Filter Cake"))+
-  labs(title="Avg. Reportable Conc.(ug/Kg) of PFAS compounds in Fields Point and Bucklin Point Filter Cake from October 2020 to May 2021")+
   ylab("Avg. Concenration (ug/Kg)")+xlab(" ")+theme(legend.position = "none")+theme(axis.text.x = element_text(angle = -90))
-#is result dependent on Fields Point or Bucklin Point
-model_fcak<-lm(textResult~Description,data=FP_BP_FCAK)
+#is result dependent on the type of PFAS compound
+model_fcak<-lm(textResult~Compound,data=FP_BP_FCAK)
+#pulling the P-value
 summaryFCAK<-get_regression_summaries(model_fcak)%>% pull(p_value)
 
 
 ############################################################################################
-# FP SIU DATA
-
+# FP significant industrial user (SIU) DATA
+#filter data by SIU and reportable average concentrations
 FP_SIU<-All_Data_pfas %>% filter(Sampling.point.type=="SIU") %>% filter(Qualifier==" ") %>% filter(Plant=="FP") %>%    
   group_by(Point.location,Compound,Plant) %>%  
   summarize(textResult= mean(textResult))
-
+#plot of Avg. Reportable Conc.(ng/L) of PFAS compounds found in Fields Point Industrial Users
 ggplot(FP_SIU,mapping=aes(Compound,textResult,color=Compound))+geom_point()+facet_wrap(~Point.location)+theme(axis.text.x = element_text(angle = -90))+theme(legend.position = "none")+
   labs(title="Avg. Reportable Conc.(ng/L) of PFAS compounds found in Fields Point Industrial Users")+
   ylab("Avg. Conc. (ng/L)")+xlab("Compound")+ theme(text = element_text(size = 8)) 
+#model to determine if there is a difference between PFAS concentrations and INdustry 
+model_FPSIU<- lm(textResult~Point.location,data=FP_SIU)
+#pull p-value
+summaryFPSIU<-get_regression_summaries(model_FPSIU) %>% pull(p_value)
 
 #################################################################################################
-#BP SIU DATA
+#BP significant industrial user (SIU) DATA
+#filter data by SIU and reportable average concentrations
 BP_SIU<-All_Data_pfas %>% filter(Sampling.point.type=="SIU") %>% filter(Qualifier==" ") %>% filter(Plant=="BP") %>%     
   group_by(Point.location,Compound,Plant) %>% 
   summarize(textResult= mean(textResult))
-
+#plot of Avg. Reportable Conc.(ng/L) of PFAS compounds found in Bucklin Point Industrial Users
 ggplot(BP_SIU,mapping=aes(Compound,textResult,color=Compound))+geom_point()+facet_wrap(~Point.location)+theme(axis.text.x = element_text(angle = -90))+theme(legend.position = "none")+
   labs(title="Avg. Reportable Conc.(ng/L) of PFAS compounds found in Bucklin Point Industrial Users")+
   ylab("Avg. Conc. (ng/L)")+xlab("Compound")+theme(text = element_text(size = 8)) 
 
+#model to determine if there is a difference between PFAS concentrations and INdustry 
+model_BPSIU<- lm(textResult~Point.location,data=BP_SIU)
+#pull p-value
+summaryBPSIU<-get_regression_summaries(model_BPSIU) %>% pull(p_value)
